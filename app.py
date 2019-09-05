675ffc0dc20919ab4792198dcc2a7d9ed4642482
Dan Debrunner <ddebrunner@users.noreply.github.com>
2019-09-05 10:27:02 -0700

    Import EndpointMonitor

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamsx.scripts.info as info
-import time
-import datetime
+
+from file_config import FileWriter
+from endpoint_monitor import EndpointMonitor
 
 info.main()
 
-from file_config import FileWriter
 cfg = FileWriter(location='/opt/streams_job_configs')
 
 em = EndpointMonitor(config=cfg, job_filter=lambda job: True)
